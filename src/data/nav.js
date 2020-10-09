45a8506d1f193b2ea5356ea71cda5487cbee91fa
Laura Padilla <lauvpadilla@gmail.com>
2020-10-09 11:56:53 -0400

    add roles and nav array

---
+++ src/data/nav.js
@@ -0,0 +1,17 @@
+const nav = [
+  {
+    children: "What is HMM?",
+    className: "order-3-ns w-100 w-30-ns mb3 mb0-ns",
+    href: "index.html",
+  },
+  {
+    children: "User Groups",
+    className: "order-1-ns w-20",
+    href: "users.html",
+  },
+  {
+    children: "Demo Site",
+    className: "order-2-ns w-20",
+    href: "dem0.html",
+  },
+];
---
+++ src/data/roles.js
@@ -0,0 +1,60 @@
+const roles = [
+  {
+    role: "External Customers",
+    description:
+      "External customers access the system to place orders, manage quality approvals, and view their data or run reports, including for environmental and regulatory compliance.",
+    className: "",
+  },
+  {
+    role: "PPG CMS Employees",
+    description:
+      "PPG CMS employees use the system to manage the inventory, receiving, labeling, filling orders, scanning and replenishing product to customer locations, and maintaining SDS and chemical data.",
+    className: "",
+  },
+  {
+    role: "Material Handlers",
+    description:
+      "They pick and fill orders, scan cabinets and customer locations, label product",
+    className: "",
+  },
+  {
+    role: "Chemical Coordinators",
+    description:
+      "receive inventory, add QC documents, print labels, process orders, ship material",
+    className: "",
+  },
+  {
+    role: "Material Coordinators",
+    description:
+      "review usage and supply chain reports in HMM (then order replenishment inventory in the ERP)",
+    className: "",
+  },
+  {
+    role: "Inventory Control Data Coordinator",
+    description:
+      "runs billing/invoicing, inventory cycle counts, inventory compare between HMM & ERP",
+    className: "",
+  },
+  {
+    role: "Chemical Manager / supervisor",
+    description:
+      "run reports, manage customer relationship, lead the handler/coordinator teams, maintain min/max & bin data.",
+    className: "",
+  },
+  {
+    role: "Implementation Supervisors",
+    description: "manage set up of new customers",
+    className: "",
+  },
+  {
+    role: "EHS Coordinators",
+    description: "maintain SDS (safety data sheets) files and chemical data",
+    className: "",
+  },
+  {
+    role: "Global admins",
+    description:
+      "view SDS, order demand items, run environmental reports, view usage and inventory reports and data",
+    className: "",
+  },
+];
